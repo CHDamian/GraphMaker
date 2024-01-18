4ff6f0c6f2520cb7aeb5e45371f4a9723f22dae8
Paweł <p.krol18a@gmail.com>
2024-01-18 16:35:13 +0100

    Created command files and declared builder methods

---
+++ add_command.h
@@ -0,0 +1,10 @@
+#ifndef GRAPHMAKER_ADD_COMMAND_H
+#define GRAPHMAKER_ADD_COMMAND_H
+
+
+class add_command {
+
+};
+
+
+#endif //GRAPHMAKER_ADD_COMMAND_H
--- builder.h
+++ builder.h
@@ -1,9 +1,34 @@
 #ifndef GRAPHMAKER_BUILDER_H
 #define GRAPHMAKER_BUILDER_H
 
+#include <memory>
+#include <stack>
+#include "graph.h"
+#include "add_command.h"
+
+using add_command_t = std::shared_ptr<add_command>;
+using command_stack_t = std::stack<add_command_t>;
+using graph_t = std::shared_ptr<graph>;
 
 class builder {
+private:
+    graph_t graph;
+    command_stack_t command_stack;
 
+    builder();
+
+public:
+    void make_spojny();
+    void make_acykliczny();
+    void make_dodatni();
+
+    void add_node(int id);
+    void add_edge(int id_1, int id_2, int weight);
+    void add_single_edge(int id_1, int id_2, int weight);
+    void undo();
+    graph_t build();
+
+    friend class facade;
 };
 
 
