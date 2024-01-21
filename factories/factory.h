77c0a9999e1738b3a4ccb74013917395e7014970
Paweł <p.krol18a@gmail.com>
2024-01-22 00:15:07 +0100

    Added comments to factories

--- factories/factory.h
+++ factories/factory.h
@@ -1,13 +1,16 @@
-//
-// Created by macie on 1/20/2024.
-//
+/** @file
+ * Deklaracja modulu fabryki
+ *
+ * @author Damian Chanko, Maciej Chanko, Pawel Krol
+ * @date 21.01.2024
+ */
 
 #ifndef GRAPHMAKER_FACTORY_H
 #define GRAPHMAKER_FACTORY_H
 #include "memory"
 #include "../graph.h"
 
-
+/**< Fabryka do tworzenia odpowiednich dekoratorow grafow */
 class factory{
 
 protected:
@@ -15,7 +18,12 @@ protected:
     factory() = default;
 
 public:
-
+    /**
+     * @brief Funkcja tworzaca udekorowany graf.
+     * Funkcja opakowuje otrzymany graf w wybrany dekorator.
+     * @param[in] graph_ptr - wskaznik do grafu, ktory nalezy udekorowac.
+     * @return Udekorowany graf.
+     */
     virtual std::shared_ptr<graph> create(std::shared_ptr<graph> graph_ptr) = 0;
 };
 
