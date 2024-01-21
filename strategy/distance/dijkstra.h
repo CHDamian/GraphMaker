4f8df6e254533601c01454d69f91e9bbd171e3b1
CHDamian <damian.chanko98@gmail.com>
2024-01-21 21:26:24 +0100

    Dijkstra

---
+++ strategy/distance/dijkstra.h
@@ -0,0 +1,33 @@
+//
+// Created by damia on 21.01.2024.
+//
+
+#ifndef GRAPHMAKER_DIJKSTRA_H
+#define GRAPHMAKER_DIJKSTRA_H
+
+#include "distance.h"
+
+class negative_edge_in_graph_exception : public std::exception{
+public:
+    [[nodiscard]] const char *what() const noexcept override {
+        return "Graph does not contain starting node!";
+    }
+};
+
+class dijkstra : public distance
+{
+    void start(graph_t graph_ptr, distance_result_t result, int start_point);
+    void detect_negative_edge(graph_t graph_ptr);
+
+public:
+    virtual distance_result_t execute(graph_t graph_ptr, int start_point);
+
+    static dijkstra& get_instance()
+    {
+        static dijkstra* instance = new dijkstra();
+        return *instance;
+    }
+};
+
+
+#endif //GRAPHMAKER_DIJKSTRA_H
